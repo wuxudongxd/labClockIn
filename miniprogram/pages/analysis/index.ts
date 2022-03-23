3971886d0d5b04a0fa62fe451995d91dc18d07aa
wuxudongxd <wuxudongxd@gmail.com>
2022-03-23 17:19:11 +0800

    ✨ feat: analysis 页面逻辑完善

--- miniprogram/pages/analysis/index.ts
+++ miniprogram/pages/analysis/index.ts
@@ -1,14 +1,31 @@
+import fetch from "../../utils/fetch";
+
 Page({
   data: {
     date: "",
+    clickInRecord: [],
+    leaveRecord: [],
     show: false,
     timestamps: [1639319824000, 1639416224000],
     minDate: new Date(2010, 0).getTime(),
     maxDate: new Date(2010, 1).getTime() - 1,
     formatter(day: any) {
-      day.bottomInfo = ".";
       return day;
     },
+  },
+  async onLoad() {
+    const { clockInResult, leaveResult } = await fetch<any>("analysis");
+    this.setData({
+      clickInRecord: clockInResult.data,
+      leaveRecord: leaveResult.data,
+      formatter: function (day: any) {
+        // data.clockInResult.data.forEach((item: any) => {
+        //   console.log(item.recordTimeStamp);
+        // });
+        day.bottomInfo = ".";
+        return day;
+      },
+    });
   },
 
   onDisplay() {
